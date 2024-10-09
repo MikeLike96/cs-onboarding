4564bea813c2d60890c38c721fb819af30c869d5
Michael <mikeyrs@hotmail.co.uk>
2024-10-09 09:03:49 +0000

    lottie animation

--- components/ImagePlaceholder.tsx
+++ components/ImagePlaceholder.tsx
@@ -1,16 +1,20 @@
-import Image from 'next/image';
+'use client';
+
+import Lottie from 'lottie-react';
+import animationData from '/Animation - email.json';
 
 export default function EmailCaptureImagePlaceholder() {
   return (
-    <div className="w-full md:w-2/5 aspect-[4/3] rounded-l-lg hidden md:flex items-center justify-center overflow-hidden">
-      <div className="relative w-full h-full p-4">
-        <Image
-          src="/cs piles 3.png"
-          alt="Email Capture Illustration"
-          fill
-          sizes="(max-width: 768px) 100vw, 40vw"
-          className="object-contain"
-          priority
+    <div className="w-full md:w-1/2 aspect-square rounded-l-lg hidden md:flex items-center justify-center overflow-hidden bg-transparent">
+      <div className="relative w-full h-full scale-120">
+        <Lottie
+          animationData={animationData}
+          loop={true}
+          autoplay={true}
+          style={{ width: '100%', height: '65%' }}
+          rendererSettings={{
+            preserveAspectRatio: 'xMidYMid slice'
+          }}
         />
       </div>
     </div>
