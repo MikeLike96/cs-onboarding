8ab2387c8389a58b1f47c52cb3f78d3decf273d4
Michael <mikeyrs@hotmail.co.uk>
2024-10-09 08:19:59 +0000

    adjusted image scaling

--- components/ImagePlaceholder.tsx
+++ components/ImagePlaceholder.tsx
@@ -2,13 +2,13 @@ import Image from 'next/image';
 
 export default function EmailCaptureImagePlaceholder() {
   return (
-    <div className="w-full md:w-2/5 aspect-[4/3] bg-[#1E2028] rounded-l-lg hidden md:flex items-center justify-center overflow-hidden">
-      <div className="relative w-[80%] h-[80%]">
+    <div className="w-full md:w-2/5 aspect-[4/3] rounded-l-lg hidden md:flex items-center justify-center overflow-hidden">
+      <div className="relative w-full h-full p-4">
         <Image
           src="/cs piles 3.png"
           alt="Email Capture Illustration"
           fill
-          sizes="(max-width: 768px) 100vw, 32vw"
+          sizes="(max-width: 768px) 100vw, 40vw"
           className="object-contain"
           priority
         />
--- components/ImagePlaceholder2.tsx
+++ components/ImagePlaceholder2.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function EmailCaptureImagePlaceholder() {
   return (
-    <div className="w-full md:w-2/5 aspect-[4/3] bg-[#1E2028] rounded-l-lg hidden md:flex items-center justify-center overflow-hidden">
+    <div className="w-full md:w-2/5 aspect-[4/3] rounded-l-lg hidden md:flex items-center justify-center overflow-hidden">
       <div className="relative w-[80%] h-[80%]">
         <Image
           src="/"
