0dbc545f2c852acb5fc3a43594e5c7aca1cf7e5a
Michael <mikeyrs@hotmail.co.uk>
2024-10-09 08:14:27 +0000

    temprary image removed

--- components/ImagePlaceholder2.tsx
+++ components/ImagePlaceholder2.tsx
@@ -5,7 +5,7 @@ export default function EmailCaptureImagePlaceholder() {
     <div className="w-full md:w-2/5 aspect-[4/3] bg-[#1E2028] rounded-l-lg hidden md:flex items-center justify-center overflow-hidden">
       <div className="relative w-[80%] h-[80%]">
         <Image
-          src="/stripe-png.png"
+          src="/"
           alt="Email Capture Illustration"
           fill
           sizes="(max-width: 768px) 100vw, 32vw"
